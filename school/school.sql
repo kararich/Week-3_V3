60c4c36cefdef8ebdb0ba1c332acfdad9e57e79f
Kara Rich <kara.rich@kellogg.northwestern.edu>
2024-01-18 02:42:25 +0000

    Part 1

--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,21 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+bio TEXT  
+);
 
+CREATE TABLE courses (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+description TEXT
+);
+
+CREATE TABLE sections (
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
